6019923ad999720d0f1591b1f73238c90f839576
Ané Cloete <cloeteane19@gmail.com>
2025-02-05 13:41:15 +0200

    more writing

--- Rscripts/variance.R
+++ Rscripts/variance.R
@@ -1,4 +1,40 @@
 
+# Define the range of F-values
+x <- seq(0, 5, length.out = 500)
+
+# Define degrees of freedom pairs
+df_pairs <- list(
+  c(1, 10),
+  c(5, 10),
+  c(10, 10),
+  c(20, 20)
+)
+
+# Define colors for different lines
+colors <- c("red", "blue", "green", "purple")
+
+# Create an empty plot
+plot(x, df(x, df_pairs[[1]][1], df_pairs[[1]][2]), type="n",
+     xlab="F value", ylab="Density",
+     main="F-distribution for Varying Degrees of Freedom")
+
+# Loop through df pairs and add lines
+for (i in seq_along(df_pairs)) {
+  lines(x, df(x, df_pairs[[i]][1], df_pairs[[i]][2]), col=colors[i], lwd=2)
+}
+
+
+
+
+
+
+
+
+# Add a legend
+legend("topright", legend=paste("df1 =", sapply(df_pairs, `[[`, 1), 
+                                ", df2 =", sapply(df_pairs, `[[`, 2)), 
+       col=colors, lwd=2, bty="n")
+
 data.val = c(1.5377, 0.6923, 1.6501, 3.7950, 5.6715, 
              2.8339, 1.5664, 6.0349, 3.8759, 3.7925,
              -1.2588, 2.3426, 3.7254, 5.4897, 5.7172,
