6a80b70b4eca995348a83f458b46b38cda3c18cf
Ané Cloete <cloeteane19@gmail.com>
2025-03-18 15:51:06 +0200

    Extra resource added

--- Rscripts/additivity.R
+++ Rscripts/additivity.R
@@ -3,26 +3,46 @@ library(dplyr)
 
 # Set parameters
 set.seed(123)
-pop.mean <- 10
-treatment.effects <- c(2, 4, 6)  # Effects for 3 treatments
-block.effects <- c(3, 2, 1)  # Effects for 3 blocks
+pop.mean <- 95
+treatment.effects <- c(6, 2, 6)  # Effects for 3 treatments
+block.effects <- c(3, 2, 1, 2,1,3)  # Effects for 3 blocks
 
 # Generate full factorial design
-treatments <- rep(1:3, each = 3)  # 3 treatments
-blocks <- rep(1:3, times = 3)  # 3 blocks
+treatments <- rep(1:3, each = 6)  # 3 treatments
+blocks <- rep(1:6, times = 3)  # 3 blocks
 
 # ---- Additive Model ----
 interaction.effects_additive <- matrix(c(
-  0, 0, 0,  
-  0, 0, 0,  
-  0, 0, 0   
-), nrow = 3, byrow = TRUE)
+  0, 0.5, 0,  
+  0.4, 0, 1.25,  
+  0.1, 1, 0,  
+  0, 0, 0.25,
+  1, 0, 0.8,
+  0, 0.6, 0
+), nrow = 6, byrow = TRUE)
 
 # Generate observed scores (Additive)
 score_additive <- pop.mean + 
   treatment.effects[treatments] + 
   block.effects[blocks] + 
   interaction.effects_additive[cbind(blocks, treatments)]
+
+add_data <- data.frame(Treat = treatments, Blocks = blocks, Response = score_additive)
+interaction.plot(add_data$Treat, add_data$Blocks, add_data$Response, lty = 1,  # Use solid lines for better visibility
+                 col = rainbow(length(unique(add_data$Blocks))),  # Use colours 
+                 lwd = 2,  # Increase line width
+                 pch = 19, # Use filled circle points
+                 cex = 1.2,  # Increase point size
+                 xlab = "Treatment", 
+                 ylab = "Mean Response", 
+                 main = "Interaction Plot: Response by Treatment and Block",
+                 legend = TRUE,  # Ensure the legend is displayed
+                 trace.label = "Block")
+
+color_data <- add_data %>% rename(Color = Treat, Area = Blocks, Texture = Response)
+color_data$Color <- factor(color_data$Color, labels = c("Red", "Black", "Blue"))
+color_data$Area <- factor(color_data$Area, labels = c("A", "B", "C", "D", "E", "F"))
+write.csv(color_data, "color_data.csv", row.names = F)
 
 # ---- Non-Additive Model ----
 interaction.effects_nonadditive <- matrix(c(
